616cd958b1651b3e1f1d33a0868b741c7138d614
Semulli <semabdulazizova@gmail.com>
2025-08-03 03:00:06 +0400

    fix-UI

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { Spin } from "antd";
 import styles from "./login.module.css";
 import LoginForm from "../../components/common/loginForm";
